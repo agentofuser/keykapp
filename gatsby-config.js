735dc4c63c6fb90a12c3b476001257f802e8d84f
Agent of User <git@agentofuser.com>
2019-07-02 17:07:48 -0400

    feat: add minimal tsx page

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        // isTSX: true,
+        // allExtensions: true,
+      },
+    },
+  ],
+}
