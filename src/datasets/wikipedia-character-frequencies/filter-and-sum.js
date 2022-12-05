b5dc04eb76a88728eb625f92c7152bbe2bbaba67
Helder S Ribeiro <helder@agentofuser.com>
2022-12-05 01:57:35 -0400

    wip

--- src/datasets/wikipedia-character-frequencies/filter-and-sum.js
+++ src/datasets/wikipedia-character-frequencies/filter-and-sum.js
@@ -3,44 +3,63 @@
 const fs = require('fs')
 
 // Read the input file
-const input = fs.readFileSync('wikipedia-character-frequencies.txt', 'utf8')
+try {
+  const input = fs.readFileSync('wikipedia-character-frequencies.txt', 'utf8')
 
-// Split the input into lines
-const lines = input.split('\n')
+  // Split the input into lines
+  const lines = input.split('\n')
 
-// Create an object to store the character frequencies
-const freqs = {}
+  // Create an object to store the character frequencies
+  const freqs = {}
 
-// Loop through each line
-for (const line of lines) {
-  // Skip the line if it's a comment or contains non-printable ASCII characters
-  if (/^#/.test(line) || /[^\x20-\x7E\n]/.test(line)) continue
+  // Loop through each line
+  for (const line of lines) {
+    // Skip the line if it's a comment or contains non-printable ASCII characters
+    if (/^#/.test(line) || /[^\x20-\x7E\n]/.test(line)) continue
 
-  // Split the line into parts
-  const parts = line.split('\t')
+    // Split the line into parts
+    const parts = line.split('\t')
 
-  // Get the character and its frequency
-  const char = parts[0]
-  const freq = parseInt(parts[2])
+    // Get the character and its frequency
+    const char = parts[0]
+    const freq = parseInt(parts[2])
 
-  // If the character is uppercase, add its frequency to the lowercase equivalent
-  if (/[A-Z]/.test(char)) {
-    // Initialize the frequency to 0 if it doesn't exist
-    if (freqs[char.toLowerCase()] === undefined) freqs[char.toLowerCase()] = 0
+    // If the character is uppercase, add its frequency to the lowercase equivalent
+    if (/[A-Z]/.test(char)) {
+      // Initialize the frequency to 0 if it doesn't exist
+      if (freqs[char.toLowerCase()] === undefined)
+        freqs[char.toLowerCase()] = 0
 
-    // Add the frequency
-    freqs[char.toLowerCase()] += freq
-  } else {
-    // Initialize the frequency to 0 if it doesn't exist
-    if (freqs[char] === undefined) freqs[char] = 0
+      // Add the frequency
+      freqs[char.toLowerCase()] += freq
+    } else {
+      // Initialize the frequency to 0 if it doesn't exist
+      if (freqs[char] === undefined) freqs[char] = 0
 
-    // Add the frequency
-    freqs[char] += freq
+      // Add the frequency
+      freqs[char] += freq
+    }
+
+    // Print the frequencies object for debugging
+    console.log(freqs)
   }
+
+  // Convert the frequencies object to a JSON string
+  try {
+    const json = JSON.stringify(freqs)
+  } catch (err) {
+    console.error(`Error converting to JSON: ${err.message}`)
+    process.exit(1)
+  }
+
+  // Write the JSON string to the output file
+  try {
+    fs.writeFileSync('output.json', json)
+  } catch (err) {
+    console.error(`Error writing output file: ${err.message}`)
+    process.exit(1)
+  }
+} catch (err) {
+  console.error(`Error reading input file: ${err.message}`)
+  process.exit(1)
 }
-
-// Convert the frequencies object to a JSON string
-const json = JSON.stringify(freqs)
-
-// Write the JSON string to the output file
-fs.writeFileSync('output.json', json)
